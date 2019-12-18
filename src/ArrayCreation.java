b7461404492a537251132e6bf3f64fc21ac9f0d5
kangbo1229 <kangbo@ssfs.org>
2019-12-18 13:45:03 -0500

    Initial commit

---
+++ src/ArrayCreation.java
@@ -0,0 +1,15 @@
+public class ArrayCreation {
+    public static double getAverage(double[] numbers) {
+        if (numbers.length < 3){
+
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        double[] numbers = {89, 75, 100};
+        System.out.println(getAverage(numbers));
+    }
+}
+
